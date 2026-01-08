83a199b10f119a49327c4035ce2556099698d82c
maxwellkoch <maxtheskier@icloud.com>
2026-01-08 10:27:18 -0700

    Day 2 in class

---
+++ in_class/rotations.m
@@ -0,0 +1,8 @@
+r0_IJK = [100;200;300]; % origin of movoing frame in IJK
+v0_IJK = [-50;30;-10]; % velocity of moving frame
+w_IJK = [1;-0.4;0.6]; % rotation of moving frame
+
+rP_IJK = [300;-100;150];
+vP_IJK = [70;25;-20];
+
+rP0_IJK = rP_IJK - r0_IJK % vector relative to moving frame
